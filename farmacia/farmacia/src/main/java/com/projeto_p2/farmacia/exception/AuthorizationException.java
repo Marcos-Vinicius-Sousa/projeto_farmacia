7de11bc10fb4778629971b9d23169254da2dfeec
Marcos Vinicius <rosario.marcosv@gmail.com>
2021-10-27 09:46:33 -0300

    criando a class AuthorizationException

---
+++ farmacia/farmacia/src/main/java/com/projeto_p2/farmacia/exception/AuthorizationException.java
@@ -0,0 +1,16 @@
+package com.projeto_p2.farmacia.exception;
+
+public class AuthorizationException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public AuthorizationException(String message) {
+		super(message);
+	}
+	
+	public AuthorizationException(String message, Throwable cause) {
+		super(message, cause);
+	}
+	
+	
+
+}
